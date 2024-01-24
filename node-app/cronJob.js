4d3f08c0ae59c82a35c6479f96cff0c03f788a9d
Chukwuemelie Obumse <emelieobumse100@gmail.com>
2024-01-24 09:31:40 +0000

    update(cron job): url called

--- node-app/cronJob.js
+++ node-app/cronJob.js
@@ -3,16 +3,16 @@ import axios from 'axios';
 import { response } from 'express';
 
 const fvtRenderHomeUrl = 'https://fvt-backend.onrender.com/api/v1/';
-const localhostHomeUrl = 'http://localhost:3000/api/v1/';
-const fvtBackendHomeUrl = localhostHomeUrl;
+//const localhostHomeUrl = 'http://localhost:3000/api/v1/';
+const fvtBackendHomeUrl = fvtRenderHomeUrl;
 
 /**
  * calls '/' endpoint every 0 seconds, 14 minutes to keep the application running 
  * this prevents the render app from sleeping after 15 minutes of inactivity 
  */
 export const job = new CronJob('0 */14 * * * *', async function () {
-  if (fvtBackendHomeUrl === localhostHomeUrl) {
-    console.log('starting job')
+  if (fvtBackendHomeUrl === fvtRenderHomeUrl) {
+    //console.log('starting job')
     try {
       const response = await axios.get(fvtBackendHomeUrl);
       if (response.status === 200) {
