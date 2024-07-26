4a611a56f194eee5033960f440dcef56f8dcc9a2
Chukwuemelie Obumse <emelieobumse100@gmail.com>
2024-07-26 20:14:32 +0100

    chore: Update HottestSongs component with MUI Divider and styling improvements

--- fvt-react-app/src/HottestSongs.js
+++ fvt-react-app/src/HottestSongs.js
@@ -1,6 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
 import Footer from './components/Footer';
+import { Divider } from '@mui/material';
 
 export function HottestSongs() {
   const [songsList, setSongsList] = useState([]);
@@ -10,26 +11,32 @@ export function HottestSongs() {
     const fetchData = async () => {
       try {
         const response = await axios.get('https://fvt-backend.onrender.com/api/v1/hottest_songs');
-        const {hottestSongs} = response.data;
-        console.log(response)
+
+        const { hottestSongs } = response.data;
+        
         if (!hottestSongs) {
           setError('Unable to fetch hottest songs');
           return;
         }
+
         const songs = Object.keys(hottestSongs).map(songRank => {
           const song = hottestSongs[songRank];
+
           return (
-            <div key={song.rank} className='p-6 w-full mb-4 lg:w-[40%] h-1/3 shadow-lg rounded-xl'>
+            <div key={song.rank} className='p-6 w-full mb-4 lg:w-[40%] h-1/3 shadow-md hover:shadow-lg border-[1px] rounded-lg space-y-2'>
               <h3><strong className='text-fvtLavender-200'>Title:</strong> {song.title}</h3>
-              <p>
-                <strong className='text-fvtLavender-200'>Artist:</strong> {song.artist}<br />
-                <strong className='text-fvtLavender-200'>Rank:</strong> {song.rank}<br />
-                <strong className='text-fvtLavender-200'>Last Week:</strong> {song['last week']}<br />
-              </p>
+              <Divider/>
+              <p><strong className='text-fvtLavender-200'>Artist:</strong> {song.artist}</p>
+              <Divider />
+              <p><strong className='text-fvtLavender-200'>Rank:</strong> {song.rank}</p>
+              <Divider />
+              <p><strong className='text-fvtLavender-200'>Last Week:</strong> {song['last week']}</p>
             </div>
           );
         });
+
         setSongsList(songs);
+        
       } catch (error) {
         setError('Error fetching data');
       }
